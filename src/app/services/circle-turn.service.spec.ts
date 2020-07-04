e9d799a712dc402e13ffd8739d607899e3cc5cb4
thanhtung060201 <thanhtung060201@gmail.com>
2020-07-04 15:46:50 +0700

    tung dang lam gio test prioritize

--- src/app/services/circle-turn.service.spec.ts
+++ src/app/services/circle-turn.service.spec.ts
@@ -27,6 +27,12 @@ describe('CircleTurnService', () => {
     expect(service.staffs[3].checkIn).toBeTrue();
   });
 
+  it('staff prioritize should order correctly', () => {
+    expect(service.staffs[1].prioritize).toBe(1);
+    expect(service.staffs[2].prioritize).toBe(2);
+    expect(service.staffs[3].prioritize).toBe(3);
+  });
+
   it('add turn 1 to staff 1', () => {
     const staffs = service.staffs;
     service.updateTurn(staffs[1].id, 'add');
